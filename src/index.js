d1158f93ff2026bb54f1cdd2c012e3e6d3ed24b0
Kazimieras Gurskas <kazimieras.gurskas@hostinger.com>
2025-11-28 11:27:18 +0200

    fix: Detect diff on new files

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ async function run() {
 
     // Split diff by files
     core.info('Splitting diff by files...');
-    const diffFiles = await splitDiffByFiles(rawDiffFile, diffDir, includeHeaders);
+    const diffFiles = await splitDiffByFiles(rawDiffFile, diffDir, includeHeaders, targetDir);
     core.info(`Generated ${diffFiles.length} diff files`);
 
     // Delete previous comments if required
